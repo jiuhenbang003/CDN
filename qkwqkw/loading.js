ddea795cc3332a9c3ad60ccd3cdb97054b61cdcd
jiuhenbang003 <63531850+jiuhenbang003@users.noreply.github.com>
2020-04-12 15:57:13 +0800

    Update loading.js

--- qkwqkw/loading.js
+++ qkwqkw/loading.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	$(".hot_banner_box").append('<div id="loading" style="position:absolute;overflow:hidden;left:0;top:0;width:100%;height: 100%;z-index:300;"><div style="position:absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index: 10;"><img style="max-width: 32px;" src="https://cdn.jsdelivr.net/gh/jiuhenbang003/cdn/qkwqkw/loading.gif"></div></div>');
+	$(".hot_banner_box").append('<div id="loading" style="position:absolute;overflow:hidden;left:0;top:0;width:100%;height: 100%;z-index:300;"><div style="position:absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index: 10;"><img style="max-width: 32px;" src="https://ae01.alicdn.com/kf/Heca3f510e677458ea35769607044d5d65.gif
+"></div></div>');
 	setTimeout(function () {
         $("#loading").remove();   
 	    $(".banner-top,.banner-wtop").addClass("opacity-top");
